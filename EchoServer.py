e94bbe4e8d87eab3f726e9ab9e125f1e418a3a93
Joshua Kim <joshuabhkim@gmail.com>
2019-06-02 14:09:32 -0400

    First commit of simple echo client and server

---
+++ EchoServer.py
@@ -0,0 +1,39 @@
+import socket
+import sys
+
+# create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind the socket to a port
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+# listen for incoming connections
+sock.listen(1)
+
+while True:
+    # wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        # receive data in small chunks and retransmit it
+        while True:
+            data = connection.recv(16)
+            print >>sys.stderr, 'received "%s"' % data
+            if data:
+                print >>sys.stderr, 'sending back to client'
+                connection.sendall(data)
+            else:
+                print >>sys.stderr, 'no more data from client', client_address
+                break
+
+    finally:
+        # clean up connection
+        connection.close()
+
+
+
